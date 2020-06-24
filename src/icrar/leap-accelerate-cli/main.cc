26bdad2d13940d19ee9fcb75d3f9ef36e5f55676
cgray <cal.j.gray@gmail.com>
2020-06-24 09:54:14 +0800

    add cli

--- src/icrar/leap-accelerate-cli/main.cc
+++ src/icrar/leap-accelerate-cli/main.cc
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <string>
+#include <CLI/CLI.hpp>
 
 int main(int argc, char** argv)
 {
+  CLI::App app { " App description " };
+  std::string filename = "default";
+  app.add_option("-f,--file", filename, "A help string");
+  try {
+      app.parse(argc, argv);
+  } catch (const CLI::ParseError& e) {
+      return app.exit(e);
+  }
+
   std::cout << " Hello World!" << std::endl;
 }
