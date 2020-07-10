68c295d2ac915ce335f3e66bab3f3fbee750890e
Callan Gray <cal.j.gray@gmail.com>
2020-07-10 14:29:50 +0800

    add kern-3 repo for xenial

--- .travis/before_install_xenial.sh
+++ .travis/before_install_xenial.sh
@@ -35,8 +35,8 @@ export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CUDA_HOME}/lib64:${CUDA_HOME}/extras/CUPTI/lib64
 export PATH=$PATH:$CUDA_HOME/bin
 
-$ sudo apt-get install software-properties-common
-$ sudo add-apt-repository -s ppa:kernsuite/kern-3
-$ sudo apt-add-repository multiverse
-$ sudo apt-add-repository restricted
-$ sudo apt-get update
+sudo apt-get install software-properties-common
+sudo add-apt-repository -s ppa:kernsuite/kern-3
+sudo apt-add-repository multiverse
+sudo apt-add-repository restricted
+sudo apt-get update
