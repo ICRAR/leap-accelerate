c5d35970cb40bd3347432c281e4c02961247fecf
Callan Gray <cal.j.gray@gmail.com>
2020-07-10 16:57:39 +0800

    add cuda key

--- .travis/before_install_xenial.sh
+++ .travis/before_install_xenial.sh
@@ -32,15 +32,11 @@ sudo apt-add-repository -y restricted
 sudo apt-get update
 sudo apt-get install casacore-dev
 
-
 wget http://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
-sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
+sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install cuda
-
-echo ls /usr/local/
-ls /usr/local/
 
 export CUDA_HOME=/usr/local/cuda
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${CUDA_HOME}/lib64:${CUDA_HOME}/extras/CUPTI/lib64
