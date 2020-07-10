92045a18e2b77091aec3107c65a39f8e9da9d06c
Callan Gray <cal.j.gray@gmail.com>
2020-07-10 15:02:17 +0800

    try http

--- .travis/before_install_xenial.sh
+++ .travis/before_install_xenial.sh
@@ -32,8 +32,9 @@ sudo apt-add-repository -y restricted
 sudo apt-get update
 sudo apt-get install casacore-dev
 
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
-sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64.deb
+
+wget http://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
 sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
 sudo apt-get update
 sudo apt-get install cuda
