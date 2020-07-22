f8ef2fac83cf7398151eaf0ac1a7d9f4dfbd7e79
Rodrigo Tobar <rtobar@icrar.org>
2020-07-22 17:22:28 +0800

    Make PhaseRotateTests compilable in non-CUDA builds
    The previous incarnation of the PhaseRotateTests assumed that CUDA was
    always available at compile time, but this will not always be the case.
    When not present we want to avoid compiling any CUDA-specific code and
    tests.
    
    This commit changes the useCuda runtime parameter used by the tests in
    the PhaseRotateTests to a compile-time argument (i.e., a template value)
    that is used to choose between the CPU and CUDA version of the test.
    This required some modification to the code, but nothing too bad. In
    turn, it allowed us to include certain test code in the compilation when
    USE_CUDA is defined.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/icrar/leap-accelerate/tests/algorithm/PhaseRotateTests.cc
+++ src/icrar/leap-accelerate/tests/algorithm/PhaseRotateTests.cc
@@ -1,8 +1,13 @@
+#include <utility>
 
 #include <gtest/gtest.h>
 
+#include <icrar/leap-accelerate/config.h>
+
 #include <icrar/leap-accelerate/algorithm/cpu/PhaseRotate.h>
+#ifdef USE_CUDA
 #include <icrar/leap-accelerate/algorithm/cuda/PhaseRotate.h>
+#endif // USE_CUDA
 
 #include <icrar/leap-accelerate/MetaData.h>
 #include <icrar/leap-accelerate/math/Integration.h>
@@ -11,6 +16,56 @@
 
 namespace icrar
 {
+
+    /// Templated version of operations implemented in both CPU and CUDA
+    template<bool use_cuda=false>
+    struct operations
+    {
+
+        template<typename ... Args>
+        static void PhaseRotate(Args && ... args)
+        {
+            icrar::cpu::PhaseRotate(std::forward<Args>(args)...);
+        }
+
+        template<typename ... Args>
+        static void RotateVisibilities(Args && ... args)
+        {
+            icrar::cpu::RotateVisibilities(std::forward<Args>(args)...);
+        }
+
+        template<typename ... Args>
+        static void PhaseMatrixFunction(Args && ... args)
+        {
+            icrar::cpu::PhaseMatrixFunction(std::forward<Args>(args)...);
+        }
+    };
+
+    #ifdef USE_CUDA
+    template<>
+    struct operations<true>
+    {
+
+        template<typename ... Args>
+        static void PhaseRotate(Args && ... args)
+        {
+            icrar::cuda::PhaseRotate(std::forward<Args>(args)...);
+        }
+
+        template<typename ... Args>
+        static void RotateVisibilities(Args && ... args)
+        {
+            icrar::cuda::RotateVisibilities(std::forward<Args>(args)...);
+        }
+
+        template<typename ... Args>
+        static void PhaseMatrixFunction(Args && ... args)
+        {
+            icrar::cuda::PhaseMatrixFunction(std::forward<Args>(args)...);
+        }
+    };
+    #endif // USE_CUDA
+
     class PhaseRotateTests : public ::testing::Test
     {
     protected:
@@ -34,7 +89,8 @@ namespace icrar
             
         }
 
-        void PhaseRotateTest(bool useCuda)
+        template<bool useCuda>
+        void PhaseRotateTest()
         {
             
             MetaData metadata;
@@ -43,33 +99,21 @@ namespace icrar
             std::queue<IntegrationResult> output_integrations;
             std::queue<CalibrationResult> output_calibrations;
 
-            if(useCuda)
-            {
-                icrar::cuda::PhaseRotate(metadata, direction, input, output_integrations, output_calibrations); //TODO: exception
-            }
-            else
-            {
-                icrar::cpu::PhaseRotate(metadata, direction, input, output_integrations, output_calibrations); //TODO: exception
-            }
+            operations<useCuda>::PhaseRotate(metadata, direction, input, output_integrations, output_calibrations); //TODO: exception
         }
 
-        void RotateVisibilitiesTest(bool useCuda)
+        template<bool useCuda>
+        void RotateVisibilitiesTest()
         {
             Integration integration;
             MetaData metadata;
             casacore::MVDirection direction;
 
-            if(useCuda)
-            {
-                icrar::cuda::RotateVisibilities(integration, metadata, direction);
-            }
-            else
-            {
-                icrar::cpu::RotateVisibilities(integration, metadata, direction); //TODO: segfault
-            }
+            operations<useCuda>::RotateVisibilities(integration, metadata, direction); //TODO: segfault with cpu
         }
 
-        void PhaseMatrixFunctionTest(bool useCuda)
+        template<bool useCuda>
+        void PhaseMatrixFunctionTest()
         {
             const casacore::Array<int32_t> a1; //TODO: populate
             const casacore::Array<int32_t> a2;
@@ -78,14 +122,7 @@ namespace icrar
 
             try
             {
-                if(useCuda)
-                {
-                    icrar::cuda::PhaseMatrixFunction(a1, a2, refAnt, map);
-                }
-                else
-                {
-                    icrar::cpu::PhaseMatrixFunction(a1, a2, refAnt, map);
-                }
+                operations<useCuda>::PhaseMatrixFunction(a1, a2, refAnt, map);
             }
             catch(std::invalid_argument& e)
             {
@@ -98,10 +135,13 @@ namespace icrar
         }
     };
 
-    TEST_F(PhaseRotateTests, DISABLED_PhaseRotateTestCpu) { PhaseRotateTest(false); }
-    TEST_F(PhaseRotateTests, DISABLED_PhaseRotateTestCuda) { PhaseRotateTest(true); }
-    TEST_F(PhaseRotateTests, DISABLED_RotateVisibilitiesTestCpu) { RotateVisibilitiesTest(false); }
-    TEST_F(PhaseRotateTests, DISABLED_RotateVisibilitiesTestCuda) { RotateVisibilitiesTest(true); }
-    TEST_F(PhaseRotateTests, DISABLED_PhaseMatrixFunctionTestCpu) { PhaseMatrixFunctionTest(false); }
-    TEST_F(PhaseRotateTests, DISABLED_PhaseMatrixFunctionTestCuda) { PhaseMatrixFunctionTest(true); }
+    TEST_F(PhaseRotateTests, DISABLED_PhaseRotateTestCpu) { PhaseRotateTest<false>(); }
+    TEST_F(PhaseRotateTests, DISABLED_RotateVisibilitiesTestCpu) { RotateVisibilitiesTest<false>(); }
+    TEST_F(PhaseRotateTests, DISABLED_PhaseMatrixFunctionTestCpu) { PhaseMatrixFunctionTest<false>(); }
+
+#ifdef USE_CUDA
+    TEST_F(PhaseRotateTests, DISABLED_PhaseRotateTestCuda) { PhaseRotateTest<true>(); }
+    TEST_F(PhaseRotateTests, DISABLED_RotateVisibilitiesTestCuda) { RotateVisibilitiesTest<true>(); }
+    TEST_F(PhaseRotateTests, DISABLED_PhaseMatrixFunctionTestCuda) { PhaseMatrixFunctionTest<true>(); }
+#endif // USE_CUDA
 }
