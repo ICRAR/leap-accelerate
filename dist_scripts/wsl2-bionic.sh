ecc740ad406876fcd15042d375b08f2a994cf6cc
Callan Gray <cal.j.gray@gmail.com>
2020-07-22 14:34:43 +0800

    precompiled header cmake version and test data

--- dist_scripts/wsl2-bionic.sh
+++
@@ -1,6 +0,0 @@
-# WSL2 CUDA repo setup
-apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
-sudo sh -c 'echo "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64 /" > /etc/apt/sources.list.d/cuda.list'
-
-sudo apt-get update
-sudo apt-get install -y cuda-toolkit-11-0
