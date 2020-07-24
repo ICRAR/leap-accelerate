d0db1d8e19c0eb472d8d9156502c2f3a9a0472a8
Callan Gray <cal.j.gray@gmail.com>
2020-07-24 03:27:29 -0700

    try cuda 10.0 bionic

--- .travis/before_install_bionic.sh
+++ .travis/before_install_bionic.sh
@@ -26,7 +26,7 @@
 #
 
 # CUDA 10.0
-wget http://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
+wget http://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb
 sudo apt-key add /var/cuda-repo-<version>/7fa2af80.pub
 sudo apt-get update
