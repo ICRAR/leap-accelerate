2f894c6f9724b05927eb4582fdc03c0fd0acc3de
Callan Gray <cal.j.gray@gmail.com>
2020-07-24 08:15:37 -0700

    cuda 10 key

--- .travis/before_install_bionic.sh
+++ .travis/before_install_bionic.sh
@@ -28,7 +28,7 @@
 # CUDA 10.0
 wget http://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
 sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64
-sudo apt-key add /var/cuda-repo-ubuntu1804-10-0-local/7fa2af80.pub
+sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
 
 # CUDA 10.1
 #wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
