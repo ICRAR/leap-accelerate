3419c55bca66ebe0e9556ee2dd0f6a931c043c40
Callan Gray <cal.j.gray@gmail.com>
2020-10-17 13:55:49 +0800

    use gcc7 on bionic

--- .travis/before_install_bionic.sh
+++ .travis/before_install_bionic.sh
@@ -58,9 +58,11 @@ export PATH=$PATH:$CUDA_HOME/bin
 sudo update-alternatives --remove-all gcc
 sudo update-alternatives --remove-all g++
 
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-7 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 20
 
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-8 10
+sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-7 10
+sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-8 20
 
 sudo update-alternatives --install /usr/bin/cc cc /usr/bin/gcc 30
 sudo update-alternatives --set cc /usr/bin/gcc
