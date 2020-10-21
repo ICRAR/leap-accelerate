b14b721b90cd54d3441302f749e0f081f16f010b
Markus Dolensky <markus.dolensky@uwa.edu.au>
2020-10-21 18:21:35 +0800

    Update submit.sh
    license

--- deploy/submit.sh
+++ deploy/submit.sh
@@ -1,13 +1,33 @@
 #!/bin/bash
 #
-# Markus Dolensky, ICRAR, 2020-10-01
+# ICRAR - International Centre for Radio Astronomy Research
+# (c) UWA - The University of Western Australia
+# Copyright by UWA(in the framework of the ICRAR)
+# All rights reserved
 #
-# Usage: sbatch leapjob.sh [LEAP_ARGS]
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+# MA 02111 - 1307  USA
+#
+
+#
+# Usage: sbatch submit.sh [LEAP_ARGS]
 #   for a list of LEAP_ARGS see the output of command LeapAccelerateCLI -h 
 #
 # examples:
-#  sbatch ./bin/leapjob.sh --help
-#  sbatch ./bin/leapjob.sh -f ~/leap-accelerate/testdata/1197638568-32.ms -s 126 -i cuda -d 
+#  sbatch ./bin/submit.sh --help
+#  sbatch ./bin/submit.sh -f ~/leap-accelerate/testdata/1197638568-32.ms -s 126 -i cuda -d 
 #[[-0.4606549305661674,-0.29719233792392513],[-0.753231018062671,-0.44387635324622354],[-0.6207547100721282,-0.2539086572881469],[-0.41958660604621867,-0.03677626900108552],[-0.41108685258900596,-0.08638012622791202],[-0.7782459495668798,-0.4887860989684432],[-0.17001324965728973,-0.28595644149463484],[-0.7129444556035118,-0.365286407171852],[-0.1512764129166089,-0.21161026349648748]]
 #  
 
