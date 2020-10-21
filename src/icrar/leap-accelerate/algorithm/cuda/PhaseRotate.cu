d0fd3e40c35f7463b89c6fd4ce52983c0ed64382
Callan Gray <00105058@uwa.edu.au>
2020-10-21 15:00:51 +0800

    cuda calibration fix

--- src/icrar/leap-accelerate/algorithm/cuda/PhaseRotate.cu
+++ src/icrar/leap-accelerate/algorithm/cuda/PhaseRotate.cu
@@ -194,19 +194,33 @@ namespace cuda
         deviceMetadata.ToHost(hostMetadata);
         
         auto avg_data_angles = hostMetadata.avg_data.unaryExpr([](std::complex<double> c) -> Radians { return std::arg(c); });
+        Eigen::VectorXi indices1 = hostMetadata.GetI1();
+        for(int& v : indices1)
+        {
+            if(v < 0)
+            {
+                v += avg_data_angles.rows(); /// -behaviour in python is to wrap around
+                //TODO: reference antenna should be included, set to 0?
+            }
+        }
 
-        Eigen::VectorXi indexes1 = hostMetadata.GetI1();
-        indexes1(indexes1.size() - 1) = 0; // TODO: check -1 behaviour, should result in 0
-        Eigen::VectorXd cal_avg_data = avg_data_angles(indexes1, 0); // 1st pol only
-        cal_avg_data(cal_avg_data.size() - 1) = 0.0; // Value at last index of cal_avg_data must be 0 (which is the reference antenna phase value)
-
+        Eigen::VectorXd cal_avg_data = avg_data_angles(indices1, 0); // 1st pol only
         auto cal1 = hostMetadata.GetAd1() * cal_avg_data;
+        // TODO: Value at last index of cal_avg_data must be 0 (which is the reference antenna phase value)
+        // cal_avg_data(cal_avg_data.size() - 1) = 0.0;
+
+        Eigen::VectorXi indices = hostMetadata.GetI();
+        for(int& v : indices)
+        {
+            if(v < 0)
+            {
+                v += avg_data_angles.rows(); // -behaviour in python is to wrap around
+                //TODO: reference antenna should be included, set to 0?
+            }
+        }
+        Eigen::MatrixXd avg_data_slice = avg_data_angles(indices, Eigen::all);
 
         Eigen::MatrixXd dInt = Eigen::MatrixXd::Zero(hostMetadata.GetI().size(), hostMetadata.avg_data.cols());
-        Eigen::VectorXi indexes = hostMetadata.GetI();
-        Eigen::MatrixXd avg_data_slice = avg_data_angles(indexes, Eigen::all);
-        avg_data_slice(avg_data_slice.rows() - 1, Eigen::all).setConstant(0.0);
-
         for(int n = 0; n < hostMetadata.GetI().size(); ++n)
         {
             Eigen::MatrixXd cumsum = hostMetadata.GetA()(n, Eigen::all) * cal1;
