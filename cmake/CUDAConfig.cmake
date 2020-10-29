15363927e11249ef241bd73016e2498a01a66f07
Callan Gray <00105058@uwa.edu.au>
2020-10-29 12:44:06 +0800

    add Werror option

--- cmake/CUDAConfig.cmake
+++ cmake/CUDAConfig.cmake
@@ -1,10 +1,13 @@
 
-# CUDA Options
-if(CUDA_VERSION_STRING VERSION_GREATER 10)
-  # 2829 annotation on a defaulted function is ignored
-  # 3057 annotation is ignored on a function that is explicitly defaulted
-  # 2929 annotation is ignored on a function that is explicitly defaulted
-  target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-Xcudafe="--diag_suppress=3057,2929">)
-else()
-  target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-Xcudafe="--diag_suppress=2829">)
-endif()
+# Configure Cuda Warning Options
+function(configure_cuda_warnings TARGET_NAME)
+  target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-Xcudafe --display_error_number>)
+  if(CUDA_VERSION_STRING VERSION_GREATER 10)
+    # 2829 annotation on a defaulted function is ignored
+    # 3057 annotation is ignored on a function that is explicitly defaulted
+    # 2929 annotation is ignored on a function that is explicitly defaulted
+    target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-Xcudafe="--diag_suppress=3057,2929">)
+  else()
+    target_compile_options(${TARGET_NAME} PRIVATE $<$<COMPILE_LANGUAGE:CUDA>:-Xcudafe="--diag_suppress=2829">)
+  endif()
+endfunction()
