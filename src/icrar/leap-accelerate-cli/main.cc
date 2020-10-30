7ab9bfd64f73553da3b6cd60415965dd0eb74a90
Rodrigo Tobar <rtobar@icrar.org>
2020-10-30 14:24:55 +0800

    Report resource usage when LeapAccelerateCLI exits
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/icrar/leap-accelerate-cli/main.cc
+++ src/icrar/leap-accelerate-cli/main.cc
@@ -33,6 +33,7 @@
 #include <icrar/leap-accelerate/core/compute_implementation.h>
 #include <icrar/leap-accelerate/core/git_revision.h>
 #include <icrar/leap-accelerate/core/logging.h>
+#include <icrar/leap-accelerate/core/resource_usage.h>
 #include <icrar/leap-accelerate/core/version.h>
 
 #include <CLI/CLI.hpp>
@@ -230,6 +231,7 @@ int main(int argc, char** argv)
         return app.exit(e);
     }
 
+    icrar::UsageReporter _;
     try
     {
         ArgumentsValidated args = ArgumentsValidated(std::move(rawArgs));
