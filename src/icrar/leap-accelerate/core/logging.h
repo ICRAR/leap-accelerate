7eecea00fd0c231e8e807604d153cea48c6957b5
Rodrigo Tobar <rtobar@icrar.org>
2020-10-30 14:20:19 +0800

    Move boost-specific headers to logging.cc
    These are needed only by the implementation of icrar::log::Initialize,
    so let's move them to logging.cc, thus removing a bit of pressure when
    building modules including logging.h.
    
    Signed-off-by: Rodrigo Tobar <rtobar@icrar.org>

--- src/icrar/leap-accelerate/core/logging.h
+++ src/icrar/leap-accelerate/core/logging.h
@@ -23,19 +23,6 @@
 #pragma once
 
 #include <boost/log/trivial.hpp>
-#include <boost/log/sources/global_logger_storage.hpp>
-#include <boost/log/sources/severity_channel_logger.hpp>
-#include <boost/log/sources/record_ostream.hpp>
-#include <boost/log/sources/severity_logger.hpp>
-#include <boost/log/utility/setup/file.hpp>
-#include <boost/log/utility/setup/common_attributes.hpp>
-#include <boost/log/utility/manipulators/add_value.hpp>
-#include <boost/log/sinks/text_file_backend.hpp>
-#include <boost/log/attributes/mutable_constant.hpp>
-
-#include <boost/log/expressions.hpp>
-#include <boost/log/core.hpp>
-#include <string>
 
 namespace icrar
 {
