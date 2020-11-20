ef560e9bfe894ca532591c0c0417025463dc2795
Callan Gray <00105058@uwa.edu.au>
2020-11-20 09:53:29 +0800

    fix xenial conditional

--- src/icrar/leap-accelerate-cli/main.cc
+++ src/icrar/leap-accelerate-cli/main.cc
@@ -82,7 +82,7 @@ int main(int argc, char** argv)
     app.add_option("-i,--implementation", rawArgs.computeImplementation, "Compute implementation type (casa, cpu, cuda)");
     //TODO: app.add_option("-m,--mwa-support", rawArgs.mwaSupport, "MWA data support by negating baselines");
 
-#if __has_include(<optsadsdional>)
+#if __has_include(<optional>)
     app.add_option("-a,--autocorrelations", rawArgs.readAutocorrelations, "Set to true if measurement set rows store autocorrelations");
     app.add_option("-m,--minimumBaselineThreshold", rawArgs.minimumBaselineThreshold, "Minimum baseline length in meters");
     app.add_option("-v,--verbosity", rawArgs.verbosity, "Verbosity (0=fatal, 1=error, 2=warn, 3=info, 4=debug, 5=trace), defaults to info");
