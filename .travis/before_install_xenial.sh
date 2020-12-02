a8aff6b3182cb062f2b0da97f7ea911bf7200c0f
Callan Gray <00105058@uwa.edu.au>
2020-12-02 09:43:04 +0800

    use -nv with wget

--- .travis/before_install_xenial.sh
+++ .travis/before_install_xenial.sh
@@ -38,7 +38,7 @@ sudo apt-get install -y casacore-dev
 # CUDA 9.0
 if ! [[ $CUDA_ENABLED == "OFF" ]]
 then
-    wget http://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
+    wget -nv http://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
     sudo dpkg -i cuda-repo-ubuntu1604-9-0-local_9.0.176-1_amd64-deb
     sudo apt-key add /var/cuda-repo-9-0-local/7fa2af80.pub
     sudo apt-get update
